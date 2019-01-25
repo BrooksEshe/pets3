b2365b7b3865ec266411f75f3d47705fb29ded9e
Brooks Eshe <beshe@mail.greenriver.edu>
2019-01-25 13:37:23 -0500

    added error reporting

--- index.php
+++ index.php
@@ -29,12 +29,12 @@ $f3->route('GET /', function() {
     echo "<a href='order'>Order a Pet</a>";
 });
 
-$f3->route('GET /order', function() {
+$f3->route('GET|POST /order', function() {
     $view = new View();
     echo $view->render('views/form1.html');
 });
 
-$f3->route('POST /order2', function() {
+$f3->route('POST|GET /order2', function() {
     $_SESSION['animal'] = $_POST['animal'];
     $template = new Template();
     echo $template->render('views/form2.html');
