ee73d169475f3315377ab3680d8720358259984c
Mike Prelesnik <mprelesnik@mail.greenriver.edu>
2019-01-25 13:50:52 -0500

    added a few functions for form validation

---
+++ model/validation-functions.php
@@ -0,0 +1,17 @@
+<?php
+
+function validColor($color)
+{
+    global $f3;
+    return in_array($color, $f3->get('colors'));
+}
+
+function validString($string)
+{
+    if(1 === preg_match('~[0-9]~', $string) OR $string == null)
+    {
+        return false;
+    }
+
+    return true;
+}
